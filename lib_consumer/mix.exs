f8a53a0e17911700d651e1620e6f2537136c2567
Adam Pash <adam.pash@gmail.com>
2018-08-30 08:46:22 -0700

    remove unnec env

--- lib_consumer/mix.exs
+++ lib_consumer/mix.exs
@@ -24,7 +24,7 @@ defmodule LibConsumer.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
-      {:phx_lib, path: "../phx_lib", env: Mix.env()}
+      {:phx_lib, path: "../phx_lib"}
     ]
   end
 end
